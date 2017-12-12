502d0ca7df2dde047cc60ccd59cfb4f8ce31a9a5
ddlee <cnlijiacheng96@gmail.com>
2017-12-12 16:40:26 +0800

    modify experiment script

--- experiments/experiment.py
+++ experiments/experiment.py
@@ -1,163 +1,208 @@
 # a experiment wrapper to run experiments
 
+
 import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
-import torch.backends.cudnn as cudnn
-
-import torchvision
 import torchvision.transforms as transforms
-
-import os
-import argparse
-
-from models import *
-from utils import progress_bar
 from torch.autograd import Variable
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torchvision import datasets
+import model
+import utils
+import time
+import argparse
+import os
+
 
 # parse args
-parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
-parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
-parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
-args = parser.parse_args()
+def parse_arg():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", type=str,
+                        default='FashionSimpleNet', help="model")
+    parser.add_argument("--patience", type=int, default=5,
+                        help="early stopping patience")
+    parser.add_argument("--batch_size", type=int, default=64, help="batch size")
+    parser.add_argument("--nepochs", type=int, default=200, help="max epochs")
+    parser.add_argument("--nocuda", action='store_true', help="no cuda used")
+    parser.add_argument("--nworkers", type=int, default=4,
+                        help="number of workers")
+    parser.add_argument("--seed", type=int, default=1, help="random seed")
+    parser.add_argument("--data", type=str, default='fashion',
+                        help="mnist or fashion")
+    args = parser.parse_args()
+    # check cuda
+    cuda = not args.nocuda and torch.cuda.is_available()  # use cuda
+    print('Training on cuda: {}'.format(cuda))
 
-use_cuda = torch.cuda.is_available()
-best_acc = 0  # best test accuracy
-start_epoch = 0  # start from epoch 0 or last checkpoint epoch
-
-# Prepare Data
-print('==> Preparing data..')
-transform_train = transforms.Compose([
-#    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-#    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
-
-transform_test = transforms.Compose([
-    transforms.ToTensor(),
-#    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
+    return args, cuda
 
 
-trainset = torchvision.datasets.FashionMNIST(root='../data', train=True, download=True, transform=transform_train)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
+def config_envs(args, cuda):
+    # Set seeds. If using numpy this must be seeded too.
+    torch.manual_seed(args.seed)
+    if cuda:
+        torch.cuda.manual_seed(args.seed)
 
-testset = torchvision.datasets.FashionMNIST(root='../data', train=False, download=True, transform=transform_test)
-testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
+    # Setup folders for saved models and logs
+    if not os.path.exists('saved-models/'):
+        os.mkdir('saved-models/')
+    if not os.path.exists('logs/'):
+        os.mkdir('logs/')
 
-classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+    # Setup tensorboard folders. Each run must have it's own folder. Creates
+    # a logs folder for each model and each run.
+    out_dir = 'logs/{}'.format(args.model)
+    if not os.path.exists(out_dir):
+        os.mkdir(out_dir)
+    run = 0
+    current_dir = '{}/run-{}'.format(out_dir, run)
+    while os.path.exists(current_dir):
+        run += 1
+        current_dir = '{}/run-{}'.format(out_dir, run)
+    os.mkdir(current_dir)
+    logfile = open('{}/log.txt'.format(current_dir), 'w')
+    print(args, file=logfile)
 
-# Model
-if args.resume:
-    # Load checkpoint.
-    print('==> Resuming from checkpoint..')
-    assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
-    checkpoint = torch.load('./checkpoint/ckpt.t7')
-    net = checkpoint['net']
-    best_acc = checkpoint['acc']
-    start_epoch = checkpoint['epoch']
-else:
-    print('==> Building model..')
-    # net = VGG('VGG19')
-    # net = ResNet18()
-    net = PreActResNet18()
-    # net = GoogLeNet()
-    # net = DenseNet121()
-    # net = ResNeXt29_2x64d()
-    # net = MobileNet()
-    # net = DPN92()
-    # net = ShuffleNetG2()
-    # net = SENet18()
+    # Tensorboard viz. tensorboard --logdir {yourlogdir}. Requires tensorflow.
+    # from tensorboard_logger import configure, log_value
+    # configure(current_dir, flush_secs=5)
+    return logfile, run
 
-if use_cuda:
-    net.cuda()
-    net = torch.nn.DataParallel(net, device_ids=range(torch.cuda.device_count()))
-    cudnn.benchmark = True
+def prepare_data(args, cuda):
+    # Prepare Data
+    print('Preparing data..')
+    train_transforms = transforms.Compose([
+        #    transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        #    transforms.Normalize((0.1307,), (0.3081,)),
+    ])
 
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
+    val_transforms = transforms.Compose([
+        transforms.ToTensor(),
+        #    transforms.Normalize((0.1307,), (0.3081,)),
+    ])
 
-# Training
-def train(epoch):
-    print('\nEpoch: %d' % epoch)
+    # Create dataloaders. Use pin memory if cuda.
+    kwargs = {'pin_memory': True} if cuda else {}
+    if(args.data == 'mnist'):
+        trainset = datasets.MNIST('data-mnist', train=True,
+                                download=True, transform=train_transforms)
+        train_loader = DataLoader(trainset, batch_size=args.batch_size,
+                                shuffle=True, num_workers=args.nworkers, **kwargs)
+        valset = datasets.MNIST('data-mnist', train=False,
+                                transform=val_transforms)
+        val_loader = DataLoader(valset, batch_size=args.batch_size,
+                                shuffle=False, num_workers=args.nworkers, **kwargs)
+    else:
+        trainset = datasets.FashionMNIST(
+            'data', train=True, download=True, transform=train_transforms)
+        train_loader = DataLoader(trainset, batch_size=args.batch_size,
+                                shuffle=True, num_workers=args.nworkers, **kwargs)
+        valset = datasets.FashionMNIST(
+            'data', train=False, transform=val_transforms)
+        val_loader = DataLoader(valset, batch_size=args.batch_size,
+                                shuffle=False, num_workers=args.nworkers, **kwargs)
+    return train_loader, val_loader
+
+def train(net, loader, criterion, optimizer, cuda):
+    '''
+    single step of training
+    '''
     net.train()
-    train_loss = 0
-    correct = 0
-    total = 0
-    for batch_idx, (inputs, targets) in enumerate(trainloader):
-        if use_cuda:
-            inputs, targets = inputs.cuda(), targets.cuda()
+    running_loss = 0
+    running_accuracy = 0
+
+    for i, (X, y) in enumerate(loader):
+        if cuda:
+            X, y = X.cuda(), y.cuda()
+        X, y = Variable(X), Variable(y)
+
+        output = net(X)
+        loss = criterion(output, y)
         optimizer.zero_grad()
-        inputs, targets = Variable(inputs), Variable(targets)
-        outputs = net(inputs)
-        loss = criterion(outputs, targets)
         loss.backward()
         optimizer.step()
+        running_loss += loss.data[0]
+        # get the index of the max log-probability
+        pred = output.data.max(1, keepdim=True)[1]
+        running_accuracy += pred.eq(y.data.view_as(pred)).cpu().sum()
+    return running_loss / len(loader), running_accuracy / len(loader.dataset)
 
-        train_loss += loss.data[0]
-        _, predicted = torch.max(outputs.data, 1)
-        total += targets.size(0)
-        correct += predicted.eq(targets.data).cpu().sum()
 
-        progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-            % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
-
-def test(epoch):
-    global best_acc
+def validate(net, loader, criterion, cuda):
     net.eval()
-    test_loss = 0
-    correct = 0
-    total = 0
-    for batch_idx, (inputs, targets) in enumerate(testloader):
-        if use_cuda:
-            inputs, targets = inputs.cuda(), targets.cuda()
-        inputs, targets = Variable(inputs, volatile=True), Variable(targets)
-        outputs = net(inputs)
-        loss = criterion(outputs, targets)
+    running_loss = 0
+    running_accuracy = 0
 
-        test_loss += loss.data[0]
-        _, predicted = torch.max(outputs.data, 1)
-        total += targets.size(0)
-        correct += predicted.eq(targets.data).cpu().sum()
-
-        progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-            % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
-
-    # Save checkpoint.
-    acc = 100.*correct/total
-    if acc > best_acc:
-        print('Saving..')
-        state = {
-            'net': net.module if use_cuda else net,
-            'acc': acc,
-            'epoch': epoch,
-        }
-        if not os.path.isdir('checkpoint'):
-            os.mkdir('checkpoint')
-        torch.save(state, './checkpoint/ckpt.t7')
-        best_acc = acc
+    for i, (X, y) in enumerate(loader):
+        if cuda:
+            X, y = X.cuda(), y.cuda()
+        X, y = Variable(X, volatile=True), Variable(y)
+        output = net(X)
+        loss = criterion(output, y)
+        running_loss += loss.data[0]
+        # get the index of the max log-probability
+        pred = output.data.max(1, keepdim=True)[1]
+        running_accuracy += pred.eq(y.data.view_as(pred)).cpu().sum()
+    return running_loss / len(loader), running_accuracy / len(loader.dataset)
 
 
-for epoch in range(start_epoch, start_epoch+200):
-    train(epoch)
-    test(epoch)
-# import datasets
+def main():
+    args, cuda = parse_arg()
+    logfile, run = config_envs(args, cuda)
+    train_loader, val_loader = prepare_data(args, cuda)
 
-# configs
+    net = model.__dict__[args.model]()
+    print(net)
+    criterion = torch.nn.CrossEntropyLoss()
 
-# choose nn model
+    if cuda:
+        net, criterion = net.cuda(), criterion.cuda()
+    # early stopping parameters
+    patience = args.patience
+    best_loss = 1e4
+
+    # Print model to logfile
+    print(net, file=logfile)
+
+    # Change optimizer for finetuning
+    optimizer = optim.Adam(net.parameters())
+
+    for e in range(args.nepochs):
+        start = time.time()
+        train_loss, train_acc = train(net, train_loader,
+            criterion, optimizer, cuda)
+        val_loss, val_acc = validate(net, val_loader, criterion, cuda)
+        end = time.time()
+
+        # print stats
+        stats ="""Epoch: {}\t train loss: {:.3f}, train acc: {:.3f}\t
+                val loss: {:.3f}, val acc: {:.3f}\t
+                time: {:.1f}s""".format( e, train_loss, train_acc, val_loss,
+                val_acc, end-start)
+        print(stats)
+        print(stats, file=logfile)
+        # tensorboard feature
+        # log_value('train_loss', train_loss, e)
+        # log_value('val_loss', val_loss, e)
+        # log_value('train_acc', train_acc, e)
+        # log_value('val_acc', val_acc, e)
+
+        #early stopping and save best model
+        if val_loss < best_loss:
+            best_loss = val_loss
+            patience = args.patience
+            utils.save_model({
+                'arch': args.model,
+                'state_dict': net.state_dict()
+            }, 'saved-models/{}-run-{}.pth.tar'.format(args.model, run))
+        else:
+            patience -= 1
+            if patience == 0:
+                print('Early stopped at epoch ' + str(e))
+                break
 
 
-
-# training
-
-
-# evaluate results
-
-
-# save log file
-
-
-# visualization
+if __name__ == '__main__':
+    main()
