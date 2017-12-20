2000353fc5148b460506655e78388ed9ff7500ff
ddlee <cnlijiacheng96@gmail.com>
2017-12-20 16:51:54 +0800

    add LeNet and GoogLeNet, tested

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,8 @@
 from .resnet import *
 from .simple import *
+from .alexnet import AlexNet
+from .squeezenet import SqueezeNet
+from .googlenet import *
+from .lenet import LeNet
 
-__all__ = ['SimpleNet', 'ResNet' 'ResNet18', 'ResNet50']
+__all__ = ['SimpleNet', 'ResNet' 'ResNet18', 'ResNet50', 'LeNet', 'AlexNet', 'SqueezeNet', 'GoogLeNet']
