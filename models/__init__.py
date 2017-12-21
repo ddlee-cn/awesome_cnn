7a8c678e8e0c2cf11921df5ed1e9538c33a0e018
ddlee <cnlijiacheng96@gmail.com>
2017-12-21 14:19:49 +0800

    add resnext and vgg, tested

--- models/__init__.py
+++ models/__init__.py
@@ -4,5 +4,9 @@ from .alexnet import AlexNet
 from .squeezenet import SqueezeNet
 from .googlenet import *
 from .lenet import LeNet
+from .inception import Inception3
+from .vgg import VGG16, VGG19
+from .resnext import ResNeXt29_2x64d, ResNeXt29_32x4d
 
-__all__ = ['SimpleNet', 'ResNet' 'ResNet18', 'ResNet50', 'LeNet', 'AlexNet', 'SqueezeNet', 'GoogLeNet']
+__all__ = ['SimpleNet', 'ResNet18', 'ResNet50', 'LeNet', 'AlexNet', 'SqueezeNet', 'GoogLeNet',
+           'Inception3', 'VGG16', 'VGG19', 'ResNeXt29_2x64d', 'ResNeXt29_32x4d']
