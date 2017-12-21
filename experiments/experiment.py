f3c2df3ab3c7f1b80532426907c7811aada525f6
ddlee <cnlijiacheng96@gmail.com>
2017-12-21 12:05:19 +0800

    add data agumentation

--- experiments/experiment.py
+++ experiments/experiment.py
@@ -81,16 +81,17 @@ def prepare_data(args, cuda):
     # Prepare Data
     print('Preparing data..')
     train_transforms = transforms.Compose([
-        #    transforms.RandomCrop(32, padding=4),
+        transforms.RandomCrop(32, padding=4),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
-        #    transforms.Normalize((0.1307,), (0.3081,)),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
 
     val_transforms = transforms.Compose([
         transforms.ToTensor(),
-        #    transforms.Normalize((0.1307,), (0.3081,)),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
+
 
     # Create dataloaders. Use pin memory if cuda.
     kwargs = {'pin_memory': True} if cuda else {}
