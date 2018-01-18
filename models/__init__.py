6998b002af3f1cb93de40dcf0eb6a6a3df8996cf
ddlee <cnlijiacheng96@gmail.com>
2018-01-18 15:04:46 +0800

    mkdir model folders and move pytorch scripts

--- models/__init__.py
+++ models/__init__.py
@@ -1,15 +1,15 @@
-from .resnet import *
-from .preact_resnet import *
+from .resnet.resnet import *
+from .resnet_v2.preact_resnet import *
 from .simple import *
-from .alexnet import AlexNet
-from .squeezenet import SqueezeNet
-from .googlenet import *
-from .lenet import LeNet
-from .inception import Inception3
-from .vgg import VGG16, VGG19
-from .resnext import ResNeXt29_2x64d, ResNeXt29_32x4d
-from .densenet import *
-from .mobilenet import MobileNet
+from .alexnet.alexnet import AlexNet
+from .squeezenet.squeezenet import SqueezeNet
+from .googlenet.googlenet import *
+from .lenet.lenet import LeNet
+from .inception.inception import Inception3
+from .vgg.vgg import VGG16, VGG19
+from .resnext.resnext import ResNeXt29_2x64d, ResNeXt29_32x4d
+from .densenet.densenet import *
+from .mobilenet.mobilenet import MobileNet
 
 __all__ = ['SimpleNet', 'ResNet18', 'ResNet50', 'LeNet', 'AlexNet', 'SqueezeNet', 'GoogLeNet',
            'Inception3', 'VGG16', 'VGG19', 'PreActResNet18', 'PreActResNet50', 'ResNeXt29_2x64d', 'ResNeXt29_32x4d',
